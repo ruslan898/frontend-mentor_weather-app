161659177500bd976902f4d3751277dacd3b3381
Ruslan Abibullaev <ruslan_abibullaev@mail.ru>
2026-02-04 19:39:52 +0500

    Add clsx dependency and remove postcss-plugin-pxtoem; update PostCSS config

--- postcss.config.js
+++ postcss.config.js
@@ -1,26 +1,15 @@
 import pxtorem from 'postcss-pxtorem';
-import postcssPxToEm from 'postcss-plugin-pxtoem';
 
 export default {
   plugins: [
     pxtorem({
       rootValue: 16,
       unitPrecision: 5,
-      propList: ['*', '!margin*', '!padding*'],
+      propList: ['*'],
       selectorBlackList: [],
       replace: true,
       mediaQuery: true,
       minPixelValue: 0,
-    }),
-    postcssPxToEm({
-      rootValue: 16,
-      unitPrecision: 5,
-      propList: ['line-height', 'letter-spacing', 'margin*', 'padding*'],
-      selectorBlackList: [],
-      replace: true,
-      mediaQuery: false,
-      minPixelValue: 0,
-      exclude: /node_modules/i,
-    }),
+    })
   ],
 };
